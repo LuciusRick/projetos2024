b4a77085277a27088fc5ee0e8a734bbbb7ebc5db
Lucius Rick Dev <124920048+LuciusRick@users.noreply.github.com>
2024-06-05 14:28:46 -0300

    transfer files

---
+++ MoreOneArt/scripts/script.js
@@ -0,0 +1,16 @@
+function mudouTamanho(){
+    if (window.innerWidth >= 768){
+        itensid.style.display = 'block'
+    } else {
+        itensid.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (itensid.style.display == 'none'){
+        itensid.style.display = 'block'
+    } else {
+        itensid.style.display = 'none'
+    }
+    
+}
